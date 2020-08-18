d97f836cc29cd87447d9bd9e9d2cdbeb6bf50706
sagulati <sagulati@amazon.com>
2020-08-18 08:37:01 -0600

    moving download to OnAfterInit

--- src/OnAfterInit.ps1
+++ src/OnAfterInit.ps1
@@ -14,3 +14,12 @@ users log in.
 #>
 
 Write-Host "Main script finished running at $(Get-Date)."
+
+Write-Information "Downloading porting assistant for .NET"
+
+(new-object net.webclient).DownloadFile('https://s3.us-west-2.amazonaws.com/aws.portingassistant.dotnet.download/latest/windows/Porting-Assistant-Dotnet.exe','C:\Users\Administrator\Downloads\Porting-Assistant-Dotnet.exe')
+
+Write-Information "Installing porting assistant for .NET"
+& C:\Users\Administrator\Downloads\Porting-Assistant-Dotnet.exe /S
+
+Write-Information "Finished installing porting assistant for .NET"
